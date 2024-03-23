99cfcac459c4d34a52616bec211fdb93b315bf92
adriano <adrianojlt@gmail.com>
2024-03-23 01:39:23 +0000

    validate pharentheses challenger

--- src/Sharping/Challenges/Challenger.cs
+++ src/Sharping/Challenges/Challenger.cs
@@ -34,5 +34,45 @@
 
             return total;
         }
+
+        public static bool validateParentheses(string value) 
+        {
+            if (string.IsNullOrWhiteSpace(value)) 
+            {
+                return false;
+            }
+
+            if (value[0] != '(' && value[^1] != ')') 
+            {
+                return false;
+            }
+
+            var count = 0;
+
+            foreach (char c in value) 
+            {
+                if (c != '(' && c != ')')
+                {
+                    return false; 
+                }
+
+                if (c != '(') 
+                {
+                    count++;
+                }
+
+                if (c != ')') 
+                {
+                    count--;
+                }
+            }
+
+            if (count == 0)
+            { 
+                return true;
+            }
+
+            return false;
+        }
     }
 }
--- tests/Sharping.Tests/Challengers/ChallengersTests.cs
+++ tests/Sharping.Tests/Challengers/ChallengersTests.cs
@@ -6,17 +6,33 @@ namespace Sharping.Tests.Challengers
     {
 
         [Theory]
-        [InlineData(new int[] { 1,4,3 }, 0)]
-        [InlineData(new int[] { 1,4,3,1 }, 1)]
+        [InlineData(new int[] { 1, 4, 3 }, 0)]
+        [InlineData(new int[] { 1, 4, 3, 1 }, 1)]
         [InlineData(new int[] { 4, 7, 3, 7, 4, 1, 8 }, 2)]
         [InlineData(new int[] { 4, 7, 3, 7, 4, 1, 8, 4 }, 2)]
         public void Pair_of_socks(int[] socks, int expected)
-        { 
+        {
             var ch = new Challenger();
 
             var pairs = ch.PairOfSocks(socks);
 
             Assert.Equal(expected, pairs);
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData(")(", false)]
+        [InlineData("()", true)]
+        [InlineData("((()))", true)]
+        [InlineData("((()()))", true)]
+        [InlineData("(()((()))()))", false)]
+        [InlineData("(()", false)]
+        public void Validate_pharentheses(string value, bool expected) 
+        { 
+            var result = Challenger.validateParentheses(value);
+
+            Assert.Equal(expected, result);
         }
     }
 }
